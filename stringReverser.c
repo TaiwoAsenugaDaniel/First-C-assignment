cefed0dd7aac1c048331a5a09885cf4430283317
TaiwoAsenugaDaniel <117589254+TaiwoAsenugaDaniel@users.noreply.github.com>
2022-11-06 16:20:59 -0100

    added some changes to this code

--- stringReverser.c
+++ stringReverser.c
@@ -1,32 +1,24 @@
-#include <stdio.h>  
-#include <string.h>  
-  
-// function definition of the revstr()  
-void reverseStr(char *str1)  
-{  
-    // declare variable  
-    int i, len, temp;  
-    len = strlen(str1); // use strlen() to get the length of str string  
-      
-    // use for loop to iterate the string   
-    for (i = 0; i < len/2; i++)  
-    {  
-        // temp variable use to temporary hold the string  
-        temp = str1[i];  
-        str1[i] = str1[len - i - 1];  
-        str1[len - i - 1] = temp;  
-    }  
-}  
-      
-    int main()  
-    {  
-        char str[50]; // size of char string  
-        printf (" Enter the string: ");  
-        gets(str); // use gets() function to take string  
-          
-        printf (" \n Before reversing the string: %s \n", str);  
-          
-        // call revstr() function   
-        reverseStr(str);  
-        printf (" After reversing the string: %s", str);  
-    }  
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  	char Str[100], RevStr[100];
+  	int i, j, len;
+ 
+  	printf("\n Please Enter any Text :  ");
+  	gets(Str);
+  	
+  	j = 0;
+  	len = strlen(Str);
+ 
+  	for (i = len - 1; i >= 0; i--)
+  	{
+  		RevStr[j++] = Str[i];
+  	}
+  	RevStr[i] = '\0';
+ 
+  	printf("\n The reversed string is = %s", RevStr);
+  	
+  	return 0;
+}
